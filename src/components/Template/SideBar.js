4fa0a3284d767473494b9e0c9f1e9a68bad4f29f
huycans <huycan_lk@yahoo.com.vn>
2021-03-16 20:46:37 -0400

    Sidebar

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -9,24 +9,18 @@ const SideBar = () => (
   <section id="sidebar">
     <section id="intro">
       <Link to="/" className="logo">
-        <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
+        <img src={`${PUBLIC_URL}/images/me.jpg`} alt="My logo" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
-        <p><a href="mailto:michael.l.dangelo@gmail.com">michael.l.dangelo@gmail.com</a></p>
+        <h2>Huy Vuong</h2>
+        <p><a href="mailto:huyvuong0096@gmail.com">huyvuong0096@gmail.com</a></p>
       </header>
     </section>
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I like building things.
-        I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni, and
-        the co-founder and CTO of <a href="https://arthena.com">Arthena</a>. Before Arthena I was
-        at <a href="https://matroid.com">Matroid</a>
-        , <a href="https://planet.com">Planet</a>
-        , <a href="https://planetaryresources.com">Planetary Resources</a>
-        , <a href="https://facebook.com">Facebook</a>
-        , and <a href="https://seds.org">SEDS</a>.
+      <p>Huy Vuong. Software Engineer. Javascript Developer.
+        Graduate student at George Mason University in Software Engineering since 2019. Formerly worked at <a href="https://rnd4u.com/">RND4U, Inc</a>
       </p>
       <ul className="actions">
         <li>
@@ -37,7 +31,7 @@ const SideBar = () => (
 
     <section id="footer">
       <ContactIcons />
-      <p className="copyright">&copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.</p>
+      <p className="copyright">&copy; Huy Vuong</p>
     </section>
   </section>
 );
