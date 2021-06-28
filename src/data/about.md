f393918ced5cea77ead98933dc57bdf340c222bd
huycans <huycan_lk@yahoo.com.vn>
2021-06-28 10:33:59 -0400

    Update aboutme and resume

--- src/data/about.md
+++ src/data/about.md
@@ -1,12 +1,11 @@
+# About me
 
-# Intro
-Hi there. Welcome to my personal page. I'm a Software Developer from Vietnam, currently studying Software Engineering at George Mason University. Here you can find my contact information, my past projects and my resume. Feel free to send me an email if you want to contact me.
+Hi there, welcome to my personal website. I'm a Software Developer from Vietnam. I graduated from George Mason University with a Masters degree in Software Engineering. Here you can find my contact information, my past projects and my resume. Feel free to send me an email if you want to contact me.
 
-# Things I like
-Coding in JavaScript. 
-Learning about new technology, especially web related.
-Fan of video games, books and movies.
-Trivia nerds. I know too much about history and geography. Too much.
+# My tech stack
+
+I specialize in MERN (MongoDB, Express, React, Node) stack. Addtional technologies that I used include Redux, TypeScript, SCSS, and Jest.
 
 # Cool stuff to check out
+
 Check out my old school [projects](/projects)
--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -5,10 +5,11 @@ const positions = [
     link: 'https://rnd4u.com/',
     daterange: 'Feb 2018 - Oct 2018',
     points: [
-      "Work in an Agile Scrum team to translate business requirements into functional code; collaborated with backend engineer to integrate frontend components using REST API",
-      "Designed and coded 15 frontend UI components in 1 month for a report - management app written in React + Redux",
-      "Demonstrated strong communication skills through daily Scrum meetings with team members and trusted with reporting work progress to customers once a month."
-    ], 
+      "Translated customers’ business requirements into functional customer-focused solutions, following industry best practices",
+      "Collaborated with product managers and backend engineers in Agile Scrum environment to integrate frontend components using REST API and JSON",
+      "Composed single-handedly 15 frontend UI components in 1 month for a report-management app written in React and Redux",
+      "Demonstrated strong communication skills through daily Scrum meetings with team members and trusted with reporting work progress to overseas customers once a month."
+    ],
   },
 ];
 
