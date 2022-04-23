cadaccd69109f24010bbe864b4ae1ce228ff5d2f
huycans <huycan_lk@yahoo.com.vn>
2022-04-23 15:58:16 -0400

    Update personal projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,43 +1,66 @@
-// TODO Add a couple lines about each project
 const data = [
   {
-    title: 'Digital Grocery',
-    link: 'https://digital-grocery.netlify.app/',
-    subtitle: 'E-commerce site designed with intuitive, user-friendly UI',
-    image: '/images/projects/grocery.png',
-    date: '2019-09-01',
+    title: "Stockviewer",
+    link: "https://stockviewer-react.herokuapp.com/",
+    subtitle: "Interactive tool for researching stocks",
+    image: "/images/projects/stockviewer_demo.png",
+    date: "2022-04-01",
     desc:
-      'An online shopping site written in React and Bootstrap. ' +
-      'Designed with a focus on intuitive, user-friendly UI. ' +
-      'Made for my UI design class. No backend is used, all data are stored on the browser. ' +
-      'https://github.com/huycans/digital-grocery'
-
+      "Get data from any public security (stock, mutual funds, ETFs) quickly. " +
+      "Written using React, Typescript, SCSS. " +
+      "Backend served by Python-based Flask server. " +
+      "Stock data provided by yFinance library. " +
+      "https://github.com/huycans/stockviewer_BE"
   },
   {
-    title: 'Secured Rummy card game',
-    link: 'https://rummy-cardgame.herokuapp.com/',
-    subtitle: 'Security-focus card game',
-    image: '/images/projects/rummy.png',
-    date: '2020-12-01',
+    title: "Youstats",
+    link: "https://youstats-react.herokuapp.com/",
+    subtitle: "Retrieve information about any Youtube channel in under 5 seconds",
+    image: "/images/projects/youstats.png",
+    date: "2021-09-01",
     desc:
-      'A security-focused Rummy card game for 2 players. ' +
-      'Written in React and NodeJS, with MongoDB database.' +
-      'This app follows most of OWASP recommendations for secured online websites. ' +
-      'Security features include: email authentication; proper user data validation; secured against SQL injection, CSRF, XSS; proper session management and data audit trail to prevent cheating. ' +
-      'Game data are transfered over secured TLS and Websocket. ' +
-      'https://github.com/huycans/Rummy-server'
+      "Simple React app that let you search for a Youtube channel and see its basic stats. " +
+      "Written using React, SCSS, Bootstraps. " +
+      "Backend served by NodeJS server, data supplied from public Youtube API. " +
+      "https://github.com/huycans/youstats"
   },
   {
-    title: 'YAMEX forum',
-    link: 'https://yamex.herokuapp.com/',
-    subtitle: 'Mortocycle-themed internet forum',
-    image: '/images/projects/yamex.png',
-    date: '2020-09-01',
+    title: "Digital Grocery",
+    link: "https://digital-grocery.netlify.app/",
+    subtitle: "E-commerce site designed with intuitive, user-friendly UI",
+    image: "/images/projects/grocery.png",
+    date: "2019-09-01",
     desc:
-      'A motorcycle theme internet forum, written in React and NodeJS and MongoDB. ' +
-      'Support account creation, authentication, threads creation, replying, and avatar uploading. ' +
-      'https://github.com/huycans/yamex-murof-backend'
+      "An online shopping site written in React and Bootstrap. " +
+      "Designed with a focus on intuitive, user-friendly UI. " +
+      "Made for my UI design class. No backend is required. " +
+      "https://github.com/huycans/digital-grocery"
   },
+  {
+    title: "Secured Rummy card game",
+    link: "https://rummy-cardgame.herokuapp.com/",
+    subtitle: "Security-focus card game",
+    image: "/images/projects/rummy.png",
+    date: "2020-12-01",
+    desc:
+      "A security-focused Rummy card game for 2 players. " +
+      "Written in React and NodeJS, with MongoDB database." +
+      "This app follows most of OWASP recommendations for secured online websites. " +
+      "Security features include: email authentication; proper user data validation; secured against SQL injection, CSRF, XSS; proper session management and data audit trail to prevent cheating. " +
+      "Game data are transfered over secured TLS and Websocket. " +
+      "https://github.com/huycans/Rummy-server"
+  },
+  {
+    title: "YAMEX forum",
+    link: "https://yamex.herokuapp.com/",
+    subtitle: "Mortocycle-themed internet forum",
+    image: "/images/projects/yamex.png",
+    date: "2020-09-01",
+    desc:
+      "A motorcycle theme internet forum, written in React and NodeJS and MongoDB. " +
+      "Support account creation, authentication, threads creation, replying, and avatar uploading. " +
+      "https://github.com/huycans/yamex-murof-backend"
+  }
 ];
 
 export default data;
